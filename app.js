be2beff514d81aedb22ab7e265589ecc239ce72b
Jeremy Green <jeremylgreen89@gmail.com>
2018-12-07 20:38:56 -0600

    small xhr method project

---
+++ app.js
@@ -0,0 +1,19 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+
+function loadData(){
+// Create an XHR Object
+const xhr = new XMLHttpRequest();
+
+//Open
+xhr.open('GET', 'file.txt', true);
+
+xhr.onload = function(){
+    if(this.status === 200){
+        document.querySelector('#output').textContent = this.responseText;
+    }
+}
+
+xhr.send();
+
+};
